44a7e3b1641e627247eb39b23f252f4e11597cc9
Iain <iain@zora.co>
2021-05-07 12:04:07 -0400

    fix metadata validation

--- tests/useNFTMetadata.test.ts
+++ tests/useNFTMetadata.test.ts
@@ -1,24 +1,26 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook } from '@testing-library/react-hooks';
 
-import fetchMock from "./setupFetchMock";
+import fetchMock from './setupFetchMock';
 
-import { useNFTMetadata } from "../src";
-import { defaultFetchAgent } from "../src/context/NFTFetchContext";
+import { useNFTMetadata } from '../src';
+import { defaultFetchAgent } from '../src/context/NFTFetchContext';
 
-describe("useNFTContent", () => {
+describe('useNFTContent', () => {
   afterEach(() => {
     defaultFetchAgent.clearCache();
     fetchMock.reset();
   });
 
-  it("loads text content for NFT from server", async () => {
-    fetchMock.get(
-      "https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE",
-      { name: "test", description: "test" },
-    );
+  it('loads text content for NFT from server', async () => {
+    fetchMock.get('https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE', {
+      name: 'test',
+      description: 'test',
+      mimeType: 'text/plain',
+      version: 'zora-20210101',
+    });
 
     const { waitFor, result } = renderHook(() =>
-      useNFTMetadata("https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE")
+      useNFTMetadata('https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE')
     );
 
     await waitFor(() => result.current.loading === false);
@@ -27,37 +29,72 @@ describe("useNFTContent", () => {
     expect(result.current.loading).toBeFalsy();
     expect(result.current.metadata).toEqual({
       description: 'test',
+      mimeType: 'text/plain',
+      version: 'zora-20210101',
       name: 'test',
     });
   });
-  it("returns error when metadata does not exist", async () => {
-    fetchMock.get("https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE", "Not Found", {
-      response: { status: 404 },
-    });
+
+  it('throws an error for invalid metadata', async () => {
+    fetchMock.get('https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE', { name: 'test' });
 
     const { waitFor, result } = renderHook(() =>
-      useNFTMetadata("https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE")
-    );
-
-    await waitFor(() => result.current.loading === false);
-
-    expect(result.current.error).toEqual("RequestError: Request Status = 404");
-    expect(result.current.loading).toBeFalsy();
-    expect(result.current.metadata).toBeUndefined();
-  });
-  it("throws exception for invalid json", async () => {
-    fetchMock.get("https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE", "INVALID JSON", {
-      response: { headers: { "content-type": "application/json" } },
-    });
-
-    const { waitFor, result } = renderHook(() =>
-      useNFTMetadata("https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE")
+      useNFTMetadata('https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE')
     );
 
     await waitFor(() => result.current.loading === false);
 
     expect(result.current.error).toEqual(
-      "RequestError: Cannot read JSON metadata from IPFS"
+      'RequestError: Metadata Invalid: Metadata could not be validated.'
+    );
+    expect(result.current.loading).toBeFalsy();
+    expect(result.current.metadata).toBeUndefined();
+  });
+
+  it('allows invalid metadata when validation exception disabled', async () => {
+    fetchMock.get('https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE', { name: 'test' });
+
+    const { waitFor, result } = renderHook(() =>
+      useNFTMetadata('https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE', { allowInvalid: true })
+    );
+
+    await waitFor(() => result.current.loading === false);
+
+    expect(result.current.error).toBeUndefined();
+    expect(result.current.loading).toBeFalsy();
+    expect(result.current.metadata).toEqual({
+      name: 'test',
+    });
+  });
+
+  it('returns error when metadata does not exist', async () => {
+    fetchMock.get('https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE', 'Not Found', {
+      response: { status: 404 },
+    });
+
+    const { waitFor, result } = renderHook(() =>
+      useNFTMetadata('https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE')
+    );
+
+    await waitFor(() => result.current.loading === false);
+
+    expect(result.current.error).toEqual('RequestError: Request Status = 404');
+    expect(result.current.loading).toBeFalsy();
+    expect(result.current.metadata).toBeUndefined();
+  });
+  it('throws exception for invalid json', async () => {
+    fetchMock.get('https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE', 'INVALID JSON', {
+      response: { headers: { 'content-type': 'application/json' } },
+    });
+
+    const { waitFor, result } = renderHook(() =>
+      useNFTMetadata('https://ipfs.io/ipfs/IPFS_SHA_EXAMPLE')
+    );
+
+    await waitFor(() => result.current.loading === false);
+
+    expect(result.current.error).toEqual(
+      'RequestError: Cannot read JSON metadata from IPFS'
     );
     expect(result.current.loading).toBeFalsy();
     expect(result.current.metadata).toBeUndefined();
