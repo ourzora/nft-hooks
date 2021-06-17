4ae4c72ab92fe2941200d9f048f5e97f227cabdb
Iain <iain@zora.co>
2021-06-17 10:23:26 -0400

    match network id enum to chainid

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -1,9 +1,11 @@
 type NetworkNames = 'MAINNET' | 'RINKEBY';
-type NetworkIDs = '1' | '2';
+type NetworkIDs = '1' | '4';
 
+// Supported networks with Zora contract deployments.
+// As more networks are supported by zora more network IDs will be added.
 const Networks: Record<NetworkNames, NetworkIDs> = {
   MAINNET: '1',
-  RINKEBY: '2',
+  RINKEBY: '4',
 };
 
 export { Networks };
