ad14780e19e24141ee7c1bae8ccde43a46b5fe93
Iain <iain@zora.co>
2021-07-15 12:56:54 -0400

    adding polygon chain id

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -1,11 +1,13 @@
-type NetworkNames = 'MAINNET' | 'RINKEBY';
-type NetworkIDs = '1' | '4';
+type NetworkNames = 'MAINNET' | 'RINKEBY' | 'POLYGON' | 'MUMBAI';
+type NetworkIDs = '1' | '4' | '137' | '80001';
 
 // Supported networks with Zora contract deployments.
 // As more networks are supported by zora more network IDs will be added.
 const Networks: Record<NetworkNames, NetworkIDs> = {
   MAINNET: '1',
   RINKEBY: '4',
+  POLYGON: '137',
+  MUMBAI: '80001',
 };
 
 export { Networks };
