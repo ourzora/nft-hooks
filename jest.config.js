2464e2eb0574567e0b904c5c63f51b75202a273b
Iain <iain@zora.co>
2021-10-15 21:13:20 -0400

    attempt to fix circleci tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   testEnvironment: 'jsdom',
   testRegex: "/tests/.*\\.(test|spec)?\\.(ts|tsx)$",
   moduleFileExtensions: ['js', 'ts', 'tsx', 'json', 'gql', 'graphql'],
+  maxWorkers: 2,
   modulePaths: ["<rootDir>/src"],
   transform: {
     "^.+\\.(j|t)sx?$": ["ts-jest"],
