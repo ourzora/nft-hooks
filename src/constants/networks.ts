a352c174283cd9bd58f09e0fbde432d149f31553
Iain Nash <iain@zora.co>
2022-01-06 16:32:03 -0500

    updating for multinetwork (#80)

--- src/constants/networks.ts
+++ src/constants/networks.ts
@@ -1,10 +1,11 @@
-type NetworkNames = 'MAINNET' | 'RINKEBY' | 'POLYGON' | 'MUMBAI';
-type NetworkIDs = '1' | '4' | '137' | '80001';
+type NetworkNames = 'MAINNET' | 'RINKEBY' | 'POLYGON' | 'MUMBAI' | 'ROPSTEN';
+type NetworkIDs = '1' | '3' | '4' | '137' | '80001';
 
 // Supported networks with Zora contract deployments.
 // As more networks are supported by zora more network IDs will be added.
 const Networks: Record<NetworkNames, NetworkIDs> = {
   MAINNET: '1',
+  ROPSTEN: '3',
   RINKEBY: '4',
   POLYGON: '137',
   MUMBAI: '80001',
